b9131bfcfa8f70fa8b0a564965c5fb13036dbfd6
Florian Knodt <git@adlerweb.info>
2018-12-07 16:24:46 +0100

    Setup: Sync after setup to ensure everything is on SD

--- setup.sh
+++ setup.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-#beware: Not tested yet
-
 echo "Enabling Camera"
 #yes, 0=on, 1=off
 sudo raspi-config nonint do_camera 0
+
 echo "Updating System"
 sudo apt update
 yes | sudo apt dist-upgrade
+
 echo "Installing prerequirements"
 yes | sudo apt install openbox xorg python3 python3-picamera python3-pyqt5 python3-pyqt5.qtwebkit lightdm python3-rpi.gpio git
 # Suggested tools: sxiv tmux vim usbmount x11vnc
+
 echo "Installing Fotobox"
 git clone https://github.com/adlerweb/fotobox.git /home/pi/fotobox
+
 echo "Configuring autostart"
 mkdir -p ~/.config/openbox
 echo "xet s off &" >> ~/.config/openbox/autostart
@@ -21,3 +23,7 @@ echo "xset s noblank &" >> ~/.config/openbox/autostart
 echo "cd ~/fotobox/ ; python3 fotobox.py | tee fotobox.log" >> ~/.config/openbox/autostart
 # GUI-Boot with autologin
 sudo raspi-config nonint do_boot_behaviour B4
+
+echo "Syncing..."
+sudo sync
+
